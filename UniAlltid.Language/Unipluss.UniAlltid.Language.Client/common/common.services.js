81f87e0c7bb9b2a9b48ef2bc4350ed78140cb95a
Christoffer Slettan <slettan@live.com>
2015-10-01 16:35:32 +0200

    Skriveleif v2

--- UniAlltid.Language/Unipluss.UniAlltid.Language.Client/common/common.services.js
+++ UniAlltid.Language/Unipluss.UniAlltid.Language.Client/common/common.services.js
@@ -6,6 +6,6 @@
 					["ngResource"])
         .constant("appSettings",
         {
-            serverPath: "http://unilanguageapi.azurewebsites.ne"
+            serverPath: "http://unilanguageapi.azurewebsites.net"
         });
 })();
