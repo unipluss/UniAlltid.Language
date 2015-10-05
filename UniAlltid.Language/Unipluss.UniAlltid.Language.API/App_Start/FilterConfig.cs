f5046dbe2ca8cbdc4ffccca39efcf660dd91c2c1
Christoffer Slettan <slettan@live.com>
2015-10-05 10:14:40 +0200

    Flere items per page, fjernet center text på tabell

--- UniAlltid.Language/Unipluss.UniAlltid.Language.API/App_Start/FilterConfig.cs
+++ UniAlltid.Language/Unipluss.UniAlltid.Language.API/App_Start/FilterConfig.cs
@@ -8,7 +8,9 @@ namespace UniAlltid.Language.API
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+#if !DEBUG
             filters.Add(new RequireHttpsAttribute());
+#endif
         }
     }
 }
